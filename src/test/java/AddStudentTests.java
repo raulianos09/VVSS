42ecf5df6869873b46d7feae1620ed4787353d18
raulianos09 <raulianos09@gmail.com>
2022-03-15 17:10:21 +0200

    initial commit

---
+++ src/test/java/AddStudentTests.java
@@ -0,0 +1,27 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddStudentTests {
+
+    @BeforeClass
+    public void doBeforeClass(){
+
+    }
+
+    @Test
+    public void addStudent_ValidData_Test(){
+
+    }
+
+    @Test
+    public void addStudent_InvalidGroupNr_Test(){
+
+    }
+
+
+    @AfterClass
+    public void doAfterClass(){
+
+    }
+}
