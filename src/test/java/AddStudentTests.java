5ab57c394368391493019e43c6563c5ade03f5c5
raulianos09 <raul_ianos@yahoo.com>
2022-03-15 17:48:31 +0200

    added 2 test cases for add student

--- src/test/java/AddStudentTests.java
+++ src/test/java/AddStudentTests.java
@@ -1,27 +1,75 @@
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
 
 public class AddStudentTests {
 
-    @BeforeClass
-    public void doBeforeClass(){
+    private String validStudentIdNr = "1000", invalidStudentIdNr = null;
+    private String validStundentName = "Raul", invalidStudentName = "";
+    private int validGroup = 934, invalidGroup = 0;
 
+    private Validator<Student> studentValidator = null;
+    private Validator<Tema> temaValidator = null;
+    private Validator<Nota> notaValidator = null;
+    private StudentXMLRepository studentXMLRepository = null;
+    private TemaXMLRepository temaXMLRepository = null;
+    private NotaXMLRepository notaXMLRepository = null;
+
+    private Service service = null;
+
+
+    @Before
+    public void doBeforeEach() {
+        this.studentValidator = new StudentValidator();
+        this.temaValidator = new TemaValidator();
+        this.notaValidator = new NotaValidator();
+
+        this.studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+        this.temaXMLRepository = new TemaXMLRepository(temaValidator, "teme.xml");
+        this.notaXMLRepository = new NotaXMLRepository(notaValidator, "note.xml");
+
+        this.service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
     }
 
     @Test
-    public void addStudent_ValidData_Test(){
-
+    public void addStudent_ValidData_Test() {
+        Student addedStudent = new Student(validStudentIdNr, validStundentName, validGroup);
+        this.service.saveStudent(validStudentIdNr,validStundentName,validGroup);
+        Iterable<Student> students = this.service.findAllStudents();
+        boolean studentWasAdded = false;
+        for(Student s : students)
+            if(s.equals(addedStudent))
+            {
+                studentWasAdded = true;
+            }
+        Assert.assertTrue(studentWasAdded);
+        this.service.deleteStudent(validStudentIdNr);
     }
 
     @Test
-    public void addStudent_InvalidGroupNr_Test(){
-
+    public void addStudent_InvalidGroupNr_Test() {
+        Student addedStudent = new Student(validStudentIdNr, validStundentName, invalidGroup);
+        this.service.saveStudent(validStudentIdNr,validStundentName,invalidGroup);
+        Iterable<Student> students = this.service.findAllStudents();
+        boolean studentWasAdded = false;
+        for(Student s : students)
+            if(s.equals(addedStudent))
+            {
+                studentWasAdded = true;
+            }
+        Assert.assertFalse(studentWasAdded);
     }
 
-
-    @AfterClass
-    public void doAfterClass(){
-
-    }
 }
